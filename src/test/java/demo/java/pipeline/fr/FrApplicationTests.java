32da665bc64027b251925a083a06b2815f4a5c95
Chris DOMINGUES <chris.domingues@epsi.fr>
2021-07-20 10:56:35 +0200

    initialisation du projet

---
+++ src/test/java/demo/java/pipeline/fr/FrApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.java.pipeline.fr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
